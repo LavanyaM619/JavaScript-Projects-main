2ac5438aee7cb513bf27d196a7c2b3c045d996ed
Lavanya.M <110652162+LavanyaM619@users.noreply.github.com>
2024-06-09 10:11:05 +0530

    Add files via upload

---
+++ W3RESOURCES JS ALGORITHMS/JS BASIC 150 QUESTIONS/JS HTML DOM QUESTIONS/dom.js
@@ -0,0 +1,46 @@
+// QUESTION 1 - Here is a sample html file with a submit button. Now modify the style of the paragraph text through javascript code.
+function clickMe(){
+    document.getElementById("firstQuestion").style.color = "red";
+}
+
+// QUESTION 2 - Write a JavaScript function to get the values of First and Last name of the following form.
+function getFormvalue(){
+  var x=document.getElementById("form");
+  for(var i=0;i<x.length;i++){
+   if(x.elements[i].value!='Submit'){  
+      document.write(x.elements[i].value);
+    }  
+   }
+}
+
+// QUESTION 3 - 3. Write a JavaScript program to set the background color of a paragraph.
+function changeBack(){
+    document.getElementById("as").style.background = "red";
+    document.getElementById("sa").style.background = "yellow";
+}
+
+// QUESTION 4 - Here is a sample html file with a submit button. Write a JavaScript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.
+function getInfos(){
+ var a = document.getElementById("khzr").href;
+ var b = document.getElementById("khzr").hreflang;   
+ var c = document.getElementById("khzr").rel; 
+ var d = document.getElementById("khzr").target; 
+ var e = document.getElementById("khzr").type; 
+ document.write(a);
+}
+
+// QUESTION 5 - Write a JavaScript function to add rows to a table.
+function extraRow(){
+    var x = document.getElementById("Table").insertRow(0);
+    var y = x.insertCell(0);
+    var z = x.insertCell(1);
+    y.innerHTML="1";
+    z.innerHTML="2";
+}
+
+// QUESTION 13 - Write a JavaScript program to get the width and height of the window (any time the window is resized).
+function getSize(){
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
+document.getElementById("widtHeight").innerHTML = "<h1>Width: " + w + " Height: " + h + "</h1>";
+}
---
+++ W3RESOURCES JS ALGORITHMS/JS BASIC 150 QUESTIONS/index.js
@@ -0,0 +1,174 @@
+// QUESTION 1,3 - Write a JavaScript program to display the current time.
+document.write(new Date());
+
+// QUESTION 2 - Write a JavaScript program to print the contents of the current window.
+function myFunction(){
+    window.print();
+};
+
+// QUESTION 4 - Write a JavaScript program to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+var a = 5;
+var b = 6;
+var c = 7;
+var perimeter = (a + b + c)/2;
+var area = Math.sqrt(perimeter*((perimeter-a)*(perimeter-b)*(perimeter-c)));
+console.log(area);
+
+// QUESTION 6 - Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar.
+function findLeapYear(year){
+    if(year % 4 === 0 && year % 100=== 0 && year % 400=== 0){
+        console.log("This is Leap Year!");
+    }
+    else{
+        console.log("This is not Leap Year!");
+    }
+}
+console.log(findLeapYear(2000));
+
+// QUESTION 7 - Write a JavaScript program to find which 1st January is being a Sunday between 2014 and 2050.
+for(var year = 2014; year <= 2050; year++){
+    var d = new Date(year, 0, 1);
+    if( d.getDay() === 0 )
+        console.log("1st January is being a Sunday  " + year);
+    }
+
+// QUESTION 8 -  Write a JavaScript program where the program takes a random integer between 1 to 10......
+const num = Math.ceil(Math.random() * 10);
+console.log(num);
+ const yourNum = prompt("Enter the Number");
+ if (yourNum == num){
+   console.log('Matched');
+   }
+  else{
+   console.log('Not matched, the number was '+ yourNum);    
+      }
+
+// QUESTION 10 - Write a JavaScript program to calculate multiplication and division of two numbers (input from user).
+function multiply(){
+        var num1 = document.getElementById("firstNum").value;
+        var num2 = document.getElementById("secondNum").value;
+        console.log(num1 * num2);
+}
+function divide(){ 
+        var num1 = document.getElementById("firstNum").value;
+        var num2 = document.getElementById("secondNum").value;
+        console.log(num1 / num2);
+}
+
+// QUESTION 11 - Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+function toCelsius(f){
+    return (5/9) * (f-32);
+}
+console.log(toCelsius(77));
+
+// QUESTION 12 - Write a JavaScript program to get the website URL (loading page).
+console.log(document.URL);
+
+// QUESTION 13 - Write a JavaScript exercise to create a variable using a user-defined name.
+var yourName = "Sanan";
+var hisName = "Amal";
+this[yourName] = hisName;
+console.log(this[yourName]);
+
+// QUESTION 14 - Write a JavaScript exercise to get the extension of a filename. 
+var fileName = "Ders.pdf";
+console.log(fileName.split('.').pop());
+
+// QUESTION 15 - Write a JavaScript program to get the difference between a given number and 13, if the number is greater than 13 return double the absolute difference.
+function doubleDiff(v){
+    if(v <= 13){
+        return (13 - v);
+    }
+    else{
+        return (v - 13) * 2;
+    }
+}
+console.log(doubleDiff(22));
+
+// QUESTION 16 - Write a JavaScript program to compute the sum of the two given integers. If the two values are same, then returns triple their sum. 
+function sumInt(a,b){
+    if(a != b){
+        return (a + b);
+    }
+    else{
+        return (a + b) * 3;
+    }
+}
+console.log(sumInt(5,6))
+
+// QUESTION 17 - Write a JavaScript program to compute the absolute difference between a specified number and 19. Returns triple their absolute difference if the specified number is greater than 19. 
+function diffNum(n){
+    if (n <= 19){
+      return (19 - n);
+    }
+    else{
+       return (n - 19) * 3;
+        }
+}
+console.log(diffNum(10));
+
+// QUESTION 18 - Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50. 
+function findThis(x, y){
+  return ((x == 50 || y == 50) || (x + y == 50));
+}
+console.log(findThis(50, 50))
+
+// QUESTION 19 - Write a JavaScript program to check whether a given integer is within 20 of 100 or 400. 
+function testFunc(x){
+    return ((Math.abs(100 - x) <= 20) ||
+       (Math.abs(400 - x) <= 20));
+}
+console.log(testFunc(31));
+
+// QUESTION 20 - Write a JavaScript program to check from two given integers, whether one is positive and another one is negative. 
+function find(x,y){
+    if((x>0 && y<0) || (x<0 && y>0)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+console.log(find(2,2));
+
+// QUESTION 21 - Write a JavaScript program to create a new string adding "Py" in front of a given string. If the given string begins with "Py" then return the original string. 
+function checker(str1){
+    if (str1 === null || str1 === undefined || str1.substring(0, 2) === 'Py'){
+      return str1;
+    }
+      return "Py" + str1;
+    }
+console.log(checker("Python"));
+
+// QUESTION 25 - Write a JavaScript program to check whether a given positive number is a multiple of 3 or a multiple of 7.
+function q25(x){
+  if (x % 3 == 0 || x % 7 == 0){
+    return true;
+  } 
+  else{
+    return false;
+  }
+}
+console.log(q25(8));
+
+// QUESTION 28 - Write a JavaScript program to check whether two given integer values are in the range 50..99 (inclusive). Return true if either of them are in the said range.
+function findRange(y,z){
+    if((y>=50 && y<=99) || (z>=50 && z<=99)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(findRange(51,57));
+
+// QUESTION 29 - Write a JavaScript program to check whether three given integer values are in the range 50..99 (inclusive). Return true if one or more of them are in the said range.
+function findRangeThree(x, y, z){
+  return (x >= 50 && x <= 99) || (y >= 50 && y <= 99) || (z >= 50 && z <= 99);
+}
+console.log(findRangeThree(50, 90, 99));
+
+
+ 
+
+
